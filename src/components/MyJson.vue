d9c03489f5659138504818080bc4071532bd3b33
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-13 11:21:47 +0530

    myjosn.

--- src/components/MyJson.vue
+++ src/components/MyJson.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+
+let person = ref({ name: 'mumbai', id: 1, counter: 100 })
+
+let increment = () => {
+  person.value.counter++
+}
 </script>
 
 <template>
   <h1>My JSON</h1>
+
+  <h1>{{ person.id }} / {{ person.name }} / {{ person.counter }}</h1>
+  <input type="button" value="Inc" @click="person.value.counter++" />
+  <input type="button" value="Inc" @click="increment" />
 </template>
