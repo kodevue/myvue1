70fea381c8afed716a06e6a4f174cd9990fa37ab
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-13 11:27:36 +0530

    vue. api.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import MyFirst from './components/MyFirst.vue'
 import MyCounter from './components/MyCounter.vue'
 import MyJson from './components/MyJson.vue'
+import MyApi from './components/MyApi.vue'
 </script>
 
 <template>
   <MyFirst />
+  <MyApi />
   <MyJson />
   <MyCounter />
 </template>
---
+++ src/components/MyApi.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+let person = ref()
+
+let makeApiCall = async () => {
+  let url = `https://jsonplaceholder.typicode.com/posts/1`
+  let res = await fetch(url)
+  let data = await res.json()
+
+  person.value = data
+}
+</script>
+
+<template>
+  <h1>My API</h1>
+  <input type="button" value="API CALL" @click="makeApiCall" />
+
+  <h1>{{ person }}</h1>
+  <h1>{{ person?.title }}</h1>
+</template>
